72f3c27bb0d05d665bae065b0e75bdaf4cd80f23
Roman Prokopenko <prokopenkor5@gmail.com>
2017-08-17 16:19:48 +0300

    getPackageXml overriden

--- src/main/java/dev/oleksii/pmanager/fldrmanager/project/structure/meta/PackageXml.java
+++ src/main/java/dev/oleksii/pmanager/fldrmanager/project/structure/meta/PackageXml.java
@@ -1,5 +1,9 @@
 package dev.oleksii.pmanager.fldrmanager.project.structure.meta;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
 import java.io.File;
 
 public final class PackageXml extends WindowsObject {
@@ -10,5 +14,14 @@ public final class PackageXml extends WindowsObject {
 
     public PackageXml(File file) {
         super(file);
+    }
+
+    @Override
+    public Element getPackageXml(Document document) {
+        Element temp = document.createElement("packagefile");
+        Node value = document.createTextNode("package.xml");
+        temp.appendChild(value);
+
+        return temp;
     }
 }
