bace8ae32c78ba19e225dd457f16cab34554c56d
Roman Prokopenko <prokopenkor5@gmail.com>
2017-08-17 16:20:45 +0300

    added folder-typeName mapping

--- src/main/java/dev/oleksii/pmanager/fldrmanager/project/structure/folder/Folder.java
+++ src/main/java/dev/oleksii/pmanager/fldrmanager/project/structure/folder/Folder.java
@@ -1,17 +1,39 @@
-package dev.oleksii.pmanager.fldrmanager.project.structure;
+package dev.oleksii.pmanager.fldrmanager.project.structure.folder;
 
+import dev.oleksii.pmanager.fldrmanager.project.structure.ChildAppendable;
+import dev.oleksii.pmanager.fldrmanager.project.structure.FileStructure;
 import dev.oleksii.pmanager.fldrmanager.project.structure.meta.WindowsObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-public final class Folder extends WindowsObject {
+public class Folder extends WindowsObject implements ChildAppendable {
+
+    private static final Map<String, String> FOLDER_NAME_TYPE_NAME_MAPPING = new HashMap<>();
+    static {
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("classes", "ApexClass");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("components", "ApexComponent");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("pages", "ApexPage");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("triggers", "ApexTrigger");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("objects", "CustomObject");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("flows", "Flow");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("staticresources", "StaticResource");
+        FOLDER_NAME_TYPE_NAME_MAPPING.put("workflows", "Workflow");
+
+    }
+
+    public static String getTypeName(String folderName) {
+        String typeName = FOLDER_NAME_TYPE_NAME_MAPPING.get(folderName);
+        return typeName;
+    }
 
     private FileStructure structure;
 
@@ -65,21 +87,28 @@ public final class Folder extends WindowsObject {
     @Override
     public Element getPackageXml(Document document) {
         Element folderElement = document.createElement("folder");
+        return createChildElements(document, folderElement);
+    }
+
+    protected Element createChildElements(Document document, Element folderElement) {
         List<Element> childElements = this.structure.values()
                                                     .stream()
                                                     .map(childElement -> childElement.getPackageXml(document))
                                                     .collect(Collectors.toList());
 
-//        List<Element> childElements = new ArrayList<>();
-//        for (WindowsObject windowsObject : this.structure.values()) {
-//            childElements.add(windowsObject.getPackageXml(document));
-//        }
-
         childElements.stream().forEach(folderElement::appendChild);
-//        for (Element childElement : childElements) {
-//            folderElement.appendChild(childElement);
-//        }
+        appendChild(folderElement, document);
+
         return folderElement;
+    }
+
+    @Override
+    public void appendChild(Element folder, Document document) {
+        Element nameElement = document.createElement("name");
+        Node nameElementValue = document.createTextNode(Folder.getTypeName(this.getName()));
+        nameElement.appendChild(nameElementValue);
+
+        folder.appendChild(nameElement);
     }
 
     @Override
