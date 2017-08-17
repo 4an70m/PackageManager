59df784c7869df54b22a56fa05c0442b4c358753
Roman Prokopenko <prokopenkor5@gmail.com>
2017-08-17 16:21:07 +0300

    added interface

--- src/main/java/dev/oleksii/pmanager/fldrmanager/project/structure/ChildAppendable.java
+++ src/main/java/dev/oleksii/pmanager/fldrmanager/project/structure/ChildAppendable.java
@@ -1,7 +1,11 @@
-package dev.oleksii.pmanager.fldrmanager.project.packagexmlgenerator.generator;
+package dev.oleksii.pmanager.fldrmanager.project.structure;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Created by new on 17.08.2017.
  */
 public interface ChildAppendable {
+    void appendChild(Element folder, Document document);
 }
