59098b86a4a892c81bfcb7e78f1a4fccbbb43b47
INV0326\new <ihor.selskyi@ctdev.io>
2017-11-24 15:16:53 +0200

    Generate Package.xml initial commit

---
+++ src/main/java/ihor_selskyi/Main.java
@@ -0,0 +1,16 @@
+package ihor_selskyi;
+
+import ihor_selskyi.generate_package_xml.PackageXmlBuilder;
+
+/**
+ * Created by new on 24.11.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws PackageXmlBuilder.PackageXmlBuilderException {
+        PackageXmlBuilder generator =  new PackageXmlBuilder(
+                "C:\\Users\\new\\Desktop\\force-meta-backup-master\\build\\metadata",
+                "C:\\Users\\new\\Desktop\\force-meta-backup-master\\build\\metadata\\package.xml",
+                "41.0");
+        generator.run();
+    }
+}
---
+++ src/main/java/ihor_selskyi/generate_package_xml/PackageXmlBuilder.java
@@ -0,0 +1,64 @@
+package ihor_selskyi.generate_package_xml;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by new on 14.11.2017.
+ */
+public class PackageXmlBuilder {
+    
+    private static final String PATH_TO_CONFIG_FILE = "C:\\Users\\new\\Desktop\\Новая папка (2)\\PackageManager\\src\\main\\java\\ihor_selskyi\\generate_package_xml\\config.json";
+    private static final String PATH_TO_GROOVY_SCRIPT_FILE = "C:\\Users\\new\\Desktop\\Новая папка (2)\\PackageManager\\src\\main\\java\\ihor_selskyi\\generate_package_xml\\GeneratePackage.groovy";
+
+    private String pathToProjectSrc;
+    private String pathToPackageXml;
+    private String version;
+
+    public PackageXmlBuilder(String pathToProjectSrc, String pathToPackageXml, String version) throws PackageXmlBuilderException {
+        this.pathToProjectSrc = pathToProjectSrc;
+        this.pathToPackageXml = pathToPackageXml;
+        this.version = version;
+        if (this.pathToPackageXml == null) throw new PackageXmlBuilderException("Path to package.xml is not defined");
+        if (this.pathToProjectSrc == null) throw new PackageXmlBuilderException("Path to project src is not defined");
+        if (this.version == null) throw new PackageXmlBuilderException("Version is not defined");
+
+    }
+
+    public void run() {
+        ClassLoader parent = PackageXmlBuilder.class.getClassLoader();
+        GroovyClassLoader loader = new GroovyClassLoader(parent);
+        Class groovyClass = null;
+        try {
+            groovyClass = loader.parseClass(new File(PATH_TO_GROOVY_SCRIPT_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        GroovyObject groovyObject = null;
+        try {
+            groovyObject = (GroovyObject) groovyClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        Object[] path = {PATH_TO_CONFIG_FILE, this.pathToProjectSrc, this.pathToPackageXml, this.version};
+        groovyObject.setProperty("args", path);
+        Object[] argz = {};
+        groovyObject.invokeMethod("run", argz);
+    }
+
+    public class PackageXmlBuilderException extends Exception {
+
+        public PackageXmlBuilderException() {
+        }
+
+        public PackageXmlBuilderException(String message) {
+            super(message);
+        }
+    }
+}
